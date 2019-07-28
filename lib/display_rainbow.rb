afea4f0dca29ebc59065beb758084920ee432d0c
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 05:10:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 puts ""
